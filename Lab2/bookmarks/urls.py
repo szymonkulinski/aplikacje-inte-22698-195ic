cd35e1aa3ea7bf93cbce261b5ce7b12175d5d293
szymonkulinski <56955430+szymonkulinski@users.noreply.github.com>
2021-11-18 22:36:58 +0100

    Update urls.py

--- Lab2/bookmarks/urls.py
+++ Lab2/bookmarks/urls.py
@@ -2,8 +2,11 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+
 
 urlpatterns = [
+    path('', include('account.urls')),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
